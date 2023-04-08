3c6f346f30d49791b3cc43ed61a4f12115b459b8
Alex Müller <alexarnimueller@protonmail.com>
2023-04-08 20:39:05 +0200

    :construction: form added to test

--- app/forms.py
+++ app/forms.py
@@ -28,4 +28,4 @@ class ReservationForm(FlaskForm):
         choices=[("26.4.", "26.4."), ("27.4.", "27.4."), ("3.5.", "3.5."), ("4.5.", "4.5.")],
         validators=[DataRequired(message="Bitte wähle ein Datum aus.")],
     )
-    submit = SubmitField("Send")
+    submit = SubmitField("OK")
